71ca96061ade10904255968776e7797bf8e92d9d
guisofa <guilherme.possani.ramos@gmail.com>
2025-04-02 00:18:08 -0300

    comparacao de saida otimizada

--- ZeroFuncao.c
+++ ZeroFuncao.c
@@ -8,30 +8,33 @@
 real_t newtonRaphson (Polinomio p, real_t x0, int criterioParada, int *it, real_t *raiz,
                       void calcPolinomio(Polinomio, real_t, real_t*, real_t*))
 {
+    real_t valorParada;
+    if (criterioParada == 1) valorParada =  EPS;
+    else if (criterioParada == 2) valorParada = ZERO;
+    else valorParada = ULPS;
+    /* 1 = erro  2 = f(x)  3 = ulps*/
+    real_t valoresCriticos[4];
+
     *it = 0;
     Double_t x; x.f = x0;
     Double_t x_next; x_next.f = x0;
     real_t func_val = 0, der_val = 0;
-    real_t erro = 0;
-    long int ulps = 0;
-
 
     calcPolinomio(p, x.f, &func_val, &der_val);
     do {
         x_next.f = x.f - func_val/der_val;
 
         (*it)++;
-        erro = fabs(x_next.f - x.f);
-        ulps = labs(x.i - x_next.i) -1;
+        valoresCriticos[1] = fabs(x_next.f - x.f);
+        valoresCriticos[3] = labs(x.i - x_next.i) -1;
         x.f = x_next.f;
         calcPolinomio(p, x.f, &func_val, &der_val);
-    } while (*it < 500 && (criterioParada == 1 ? (erro > EPS) : 1) && (criterioParada == 3 ? (ulps > ULPS) : 1) && (criterioParada == 2 ? (fabs(func_val) > ZERO) : 1));
+        valoresCriticos[2] = fabs(func_val);
+    } while (*it < 500 && valoresCriticos[criterioParada] > valorParada);
 
     *raiz = x.f;
-    
-    if (criterioParada == 1) return erro;
-    else if (criterioParada == 2) return func_val;
-    else return ulps;
+    if (criterioParada == 3) return (valoresCriticos[3] < 0) ? 0 : valoresCriticos[3];
+    else return fabs(valoresCriticos[criterioParada]);
 }
 
 
@@ -39,13 +42,18 @@ real_t newtonRaphson (Polinomio p, real_t x0, int criterioParada, int *it, real_
 real_t bisseccao (Polinomio p, real_t a, real_t b, int criterioParada, int *it, real_t *raiz,
                   void calcPolinomio(Polinomio, real_t, real_t*, real_t*))
 {
+    real_t valorParada;
+    if (criterioParada == 1) valorParada =  EPS;
+    else if (criterioParada == 2) valorParada = ZERO;
+    else valorParada = ULPS;
+    /* 1 = erro  2 = f(x)  3 = ulps*/
+    real_t valoresCriticos[4];
+
     *it = 0;
     Double_t xm, x_next;
     real_t func_val_a = 0, func_val_m = 0;
-    real_t erro = 0;
-    long int ulps = 0;
 
-    xm.f = (a + b)/2;
+    xm.f = a/2 + b/2;
     calcPolinomio(p, xm.f, &func_val_m, NULL);
     do {
         calcPolinomio(p, a, &func_val_a, NULL);
@@ -55,21 +63,21 @@ real_t bisseccao (Polinomio p, real_t a, real_t b, int criterioParada, int *it, 
         else
             a = xm.f;
 
-        x_next.f = (a + b)/2;
+        x_next.f = a/2 + b/2;
         (*it)++;
-        erro = fabs(x_next.f - xm.f);
-        ulps = labs(xm.i - x_next.i) -1;
+        valoresCriticos[1] = fabs(x_next.f - xm.f);
+        valoresCriticos[3] = labs(xm.i - x_next.i) -1;
         xm.f = x_next.f;
         calcPolinomio(p, xm.f, &func_val_m, NULL);
-    } while (*it < 500 && (criterioParada == 1 ? (erro > EPS) : 1) && (criterioParada == 3 ? (ulps > ULPS) : 1) && (criterioParada == 2 ? (fabs(func_val_m) > ZERO) : 1));
+        valoresCriticos[2] = fabs(func_val_m);
+        /*printf("%d: %.16e, %.16e\n", *it, xm.f ,func_val_m);*/
+    } while (*it < 500 && valoresCriticos[criterioParada] > valorParada);
 
     *raiz = xm.f;
     
-    if (criterioParada == 1) return erro;
-    else if (criterioParada == 2) return func_val_m;
-    else return ulps;
+    if (criterioParada == 3) return (valoresCriticos[3] < 0) ? 0 : valoresCriticos[3];
+    else return fabs(valoresCriticos[criterioParada]);
 }
-
 
 void calcPolinomio_rapido(Polinomio p, real_t x, real_t *px, real_t *dpx)
 {
@@ -77,7 +85,7 @@ void calcPolinomio_rapido(Polinomio p, real_t x, real_t *px, real_t *dpx)
 
     for (int i = p.grau; i > 0; i--) {
         res_fun = res_fun * x + p.p[i];
-        res_der = res_der * x + p.p[i] * i;
+        res_der = res_der * x + res_fun;
     }
     res_fun = res_fun * x + p.p[0];
 
@@ -85,7 +93,6 @@ void calcPolinomio_rapido(Polinomio p, real_t x, real_t *px, real_t *dpx)
     if (dpx)
         *dpx = res_der;
 }
-
 
 void calcPolinomio_lento(Polinomio p, real_t x, real_t *px, real_t *dpx)
 {
--- labZero.c
+++ labZero.c
@@ -8,12 +8,13 @@
 
 int main ()
 {
+	// arredondamento para baixo
 	fesetround(FE_DOWNWARD);
 
 	real_t a, b;
   	Polinomio pol;
 
-  	scanf("%d", &pol.grau);
+  	scanf("%d", &pol.grau); // grau do polinomio
 	pol.p = malloc(sizeof(real_t) * (pol.grau+1));
 
   	for (int i=pol.grau; i >=0; --i)
@@ -26,26 +27,26 @@ int main ()
 	double tempo;
 
 	printf("RAPIDO\n\n");
-	for (int i = 1; i <= 3; i++) {
+	for (int i = 1; i <= 3; i++) { // bisseccao rapido
 		tempo = timestamp();
 		real_t valor_crit = bisseccao(pol, a, b, i, &iteracoes, &raiz, calcPolinomio_rapido);
 		tempo = timestamp() - tempo;
 		printf("bissec %.15e %.15e %d %.8e\n", raiz, valor_crit, iteracoes, tempo);
 	}
-	for (int i = 1; i <= 3; i++) {
+	for (int i = 1; i <= 3; i++) { // newton-raphson rapido
 		tempo = timestamp();
 		real_t valor_crit = newtonRaphson(pol, (a + b)/2, i, &iteracoes, &raiz, calcPolinomio_rapido);
 		tempo = timestamp() - tempo;
 		printf("newton %.15e %.15e %d %.8e\n", raiz, valor_crit, iteracoes, tempo);
 	}
 	printf("\nLENTO\n\n");
-	for (int i = 1; i <= 3; i++) {
+	for (int i = 1; i <= 3; i++) { // bisseccao lento
 		tempo = timestamp();
 		real_t valor_crit = bisseccao(pol, a, b, i, &iteracoes, &raiz, calcPolinomio_lento);
 		tempo = timestamp() - tempo;
 		printf("bissec %.15e %.15e %d %.8e\n", raiz, valor_crit, iteracoes, tempo);
 	}
-	for (int i = 1; i <= 3; i++) {
+	for (int i = 1; i <= 3; i++) { // newton-raphson lento
 		tempo = timestamp();
 		real_t valor_crit = newtonRaphson(pol, (a + b)/2, i, &iteracoes, &raiz, calcPolinomio_lento);
 		tempo = timestamp() - tempo;
